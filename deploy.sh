7e70b93c4a8893bc41019dfbea95a166c62c48b5
sam <parktheredcar@gmail.com>
2021-12-14 11:51:51 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 git checkout --orphan gh-pages
 yarn build
-git --work-tree add -all
+git --work-tree add --all
 git --work-tree commit -m gh-pages
 git push origin HEAD:gh-pages --force
+git checkout -f master
+git branch -D gh-pages
